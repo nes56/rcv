adfb2feb173ac2eb9cacef8f0093ce821972aa4d
Harelon <harel.mspc@gmail.com>
2019-02-13 22:54:06 +0200

    added pictures folder implemented first and second quadrants added the y leaning angle and x turning angle into account

--- measure_distance_on_mouse_click.py
+++ measure_distance_on_mouse_click.py
@@ -6,14 +6,14 @@ import numpy as np
 import os
 import sys
 
+
 # GLOBALS #
-# the below 2 globals were calculated using  calculate_pixels_per_degree.py utility
+# the below 2 globals were calculated using calculate_pixels_per_degree.py utility
 X_PIXELS_PER_DEGREE = {640: 12.452453991885616, 320: 6.226226995942808}
 Y_PIXELS_PER_DEGREE = {480: 12.216701907035985, 240: 6.108350953517992}
 image_cleared = True
 image_orig = None
 image_copy = None
-camera_height = 43.3
 
 
 class XOrientation(Enum):
@@ -62,19 +62,22 @@ def draw_measure_box(x, y):
             q = 1
         if x_orientation.value < y_orientation.value:
             q = 3
-    if q <= 2:
-        raise NotImplemented
     # drawing the box
     top_left = None
     bottom_right = None
-    if q == 3:
+    # implemented quadrants 1 and 2
+    if q == 1:
+        top_left = (int(x_center), int(y))
+        bottom_right = (int(x), int(y_center))
+    elif q == 2:
+        top_left = (int(x), int(y))
+        bottom_right = (int(x_center), int(y_center))
+    elif q == 3:
         top_left = (int(x), int(y_center))
         bottom_right = (int(x_center), int(y))
     elif q == 4:
         top_left = (int(x_center), int(y_center))
         bottom_right = (int(x), int(y))
-    else:
-        raise NotImplemented
     print("top_left = {}".format(top_left))
     print("bottom_right = {}".format(bottom_right))
     image_copy = cv2.rectangle(image_copy, top_left, bottom_right,
@@ -83,8 +86,14 @@ def draw_measure_box(x, y):
     dx = abs(x - x_center)
     print("dx = {}".format(dx))
     print("dy = {}".format(dy))
-    x_diviation_degree = (float(dx)/float(x_pixels_per_degree))
-    y_diviation_degree = (float(dy)/float(y_pixels_per_degree))
+    # adding the turning angle to the angle calculated by pixels
+    x_diviation_degree = x_turning_angle + (
+        float(dx) / float(x_pixels_per_degree)
+        )
+    # adding the y leaning angle to the angle calculated by pixels
+    y_diviation_degree = float(y_leaning_angle) + (
+        float(dy) / float(y_pixels_per_degree)
+        )
     print("x_diviation_degree = {}".format(x_diviation_degree))
     print("y_diviation_degree = {}".format(y_diviation_degree))
     # calculating distance from camera base to (x_center, y)
@@ -92,8 +101,6 @@ def draw_measure_box(x, y):
     # calculating distance from camera base to (x,y)
     d_to_x_y = d_to_x_center_y / math.cos(math.radians(x_diviation_degree))
     print("distance is {}".format(d_to_x_y))
-    # miltiply the distance  with constant (guess work) to adjust the distance
-    print("adjusted distance is  * 0.785 = {}".format(d_to_x_y * 0.785))
 
 
 def mouse_callback(event, x, y, flags, param):
@@ -136,19 +143,31 @@ def do_work(img):
 
 def main():
     global camera_height
+    global y_leaning_angle
+    global x_turning_angle
     parser_description = "Performs distance calculation to the x,y where the \
         left mouse was clicked"
     parser = argparse.ArgumentParser(description=parser_description)
     parser.add_argument('--image', type=str, required=True,
                         help='path of the image to process')
-    parser.add_argument('--camera-height', type=float, default=43.3,
-                        help="the height of camera when the picture was taken \
-                        when the picture was taken")
+    parser.add_argument('--camera-height', type=float, default=38.5,
+                        help="the height of camera when the picture was taken")
+    parser.add_argument('--y_leaning_angle', type=float, default=28,
+                        help="the y angle of camera when the picture was taken"
+                        )
+    parser.add_argument('--x_turning_angle', type=float, default=1.9,
+                        help="the difference in pixels between the center line\
+                         and a straight reference line\
+                         when the picture was taken\
+                         positive angle is to the right"
+                        )
     args = parser.parse_args()
     if not os.path.isfile(args.image):
         print('Please provide a valid path to an image file')
         sys.exit(-1)
     camera_height = args.camera_height
+    y_leaning_angle = args.y_leaning_angle
+    x_turning_angle = args.x_turning_angle
     do_work(args.image)
 
 
