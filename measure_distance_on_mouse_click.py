6638756f075e67a49fd46891310fe463fcd57048
Harel <harel.games@gmail.com>
2019-02-14 07:39:11 +0200

    moved the constants y and x pixels per degree to another file

--- measure_distance_on_mouse_click.py
+++ measure_distance_on_mouse_click.py
@@ -5,12 +5,9 @@ import math
 import numpy as np
 import os
 import sys
+from constants import X_PIXELS_PER_DEGREE, Y_PIXELS_PER_DEGREE
 
 
-# GLOBALS #
-# the below 2 globals were calculated using calculate_pixels_per_degree.py utility
-X_PIXELS_PER_DEGREE = {640: 12.452453991885616, 320: 6.226226995942808}
-Y_PIXELS_PER_DEGREE = {480: 12.216701907035985, 240: 6.108350953517992}
 image_cleared = True
 image_orig = None
 image_copy = None
