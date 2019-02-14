d7f61189614eb58a2c648608c835d5c2f44b364c
Harel <harel.games@gmail.com>
2019-02-14 07:51:45 +0200

    moved finding the distance to another function

--- measure_distance_on_mouse_click.py
+++ measure_distance_on_mouse_click.py
@@ -25,20 +25,47 @@ class YOrientation(Enum):
     down = 1
 
 
-def draw_measure_box(x, y):
-    global image_copy
-    height, width = image_copy.shape[0:2]
+def find_distance_by_coordinates(
+    x, y, image_shape, camera_height, y_leaning_angle, x_turning_angle
+):
+    height, width = image_shape[0:2]
+    x_pixels_per_degree = X_PIXELS_PER_DEGREE[width]
+    y_pixels_per_degree = Y_PIXELS_PER_DEGREE[height]
     print("***********************************")
     print("camera_height = {}".format(camera_height))
     print("x = {} , y = {}".format(x, y))
-    x_center = width/2
-    y_center = height/2
-    x_pixels_per_degree = X_PIXELS_PER_DEGREE[width]
-    y_pixels_per_degree = Y_PIXELS_PER_DEGREE[height]
     print("x_pixels_per_degree = {}".format(x_pixels_per_degree))
     print("y_pixels_per_degree = {}".format(y_pixels_per_degree))
+    x_center = width/2
+    y_center = height/2
+    dy = y - y_center
+    dx = abs(x - x_center)
+    print("dx = {}".format(dx))
+    print("dy = {}".format(dy))
+    # adding the turning angle to the angle calculated by pixels
+    x_diviation_degree = x_turning_angle + (
+        float(dx) / float(x_pixels_per_degree)
+        )
+    # adding the y leaning angle to the angle calculated by pixels
+    y_diviation_degree = float(y_leaning_angle) + (
+        float(dy) / float(y_pixels_per_degree)
+        )
+    print("x_diviation_degree = {}".format(x_diviation_degree))
+    print("y_diviation_degree = {}".format(y_diviation_degree))
+    # calculating distance from camera base to (x_center, y)
+    d_to_x_center_y = camera_height/math.tan(math.radians(y_diviation_degree))
+    # calculating distance from camera base to (x,y)
+    d_to_x_y = d_to_x_center_y / math.cos(math.radians(x_diviation_degree))
+    return d_to_x_center_y
+
+
+def draw_measure_box(x, y):
+    global image_copy
+    height, width = image_copy.shape[0:2]        
     x_orientation = XOrientation.center
     y_orientation = YOrientation.center
+    x_center = width/2
+    y_center = height/2
     if x > x_center:
         x_orientation = XOrientation.right
     elif x < x_center:
@@ -79,25 +106,10 @@ def draw_measure_box(x, y):
     print("bottom_right = {}".format(bottom_right))
     image_copy = cv2.rectangle(image_copy, top_left, bottom_right,
                                (0, 255, 0), 2)
-    dy = y - y_center
-    dx = abs(x - x_center)
-    print("dx = {}".format(dx))
-    print("dy = {}".format(dy))
-    # adding the turning angle to the angle calculated by pixels
-    x_diviation_degree = x_turning_angle + (
-        float(dx) / float(x_pixels_per_degree)
+    distance = find_distance_by_coordinates(
+        x, y, image_copy.shape, camera_height, y_leaning_angle, x_turning_angle
         )
-    # adding the y leaning angle to the angle calculated by pixels
-    y_diviation_degree = float(y_leaning_angle) + (
-        float(dy) / float(y_pixels_per_degree)
-        )
-    print("x_diviation_degree = {}".format(x_diviation_degree))
-    print("y_diviation_degree = {}".format(y_diviation_degree))
-    # calculating distance from camera base to (x_center, y)
-    d_to_x_center_y = camera_height/math.tan(math.radians(y_diviation_degree))
-    # calculating distance from camera base to (x,y)
-    d_to_x_y = d_to_x_center_y / math.cos(math.radians(x_diviation_degree))
-    print("distance is {}".format(d_to_x_y))
+    print("distance is {}".format(distance))
 
 
 def mouse_callback(event, x, y, flags, param):
