68e3c3809833183d704bc81b73046e64bb7c6930
Barak Azulay <bazulay@gmail.com>
2019-02-18 20:20:06 +0200

    Fixed the typo in the service file and improved install & uninstall scripts
    Signed-off-by: Barak Azulay <bazulay@gmail.com>

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
+#disabling rcv.service (to not try and start the service on boot)
+sudo systemctl disable rcv.service
+
 #removing the rcv service 
-sudo rm -f /lib/systemd/system/rcv.service 
+sudo rm -f /lib/systemd/system/rcv.service
+
+#removing the symbolic link from /opt if exists
+if [ -L "/opt/rcv" ]; then
+    sudo rm -f /opt/rcv
+fi
