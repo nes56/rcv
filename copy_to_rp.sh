c82e652c1e0a6d3cf6191f614f15b1a45fbaf112
Harel <harel.games@gmail.com>
2019-04-02 01:22:53 +0300

    added install and removing daemon log to install script
    Signed-off-by: Harel Bar <harel.games@gmail.com>

--- copy_to_rp.sh
+++ copy_to_rp.sh
@@ -3,9 +3,11 @@
 sudo tar -czf rcv.tar.gz --exclude=".git" --exclude="*.mp4" --exclude="*.avi" --exclude="__pycache__" --exclude="*.pyc" -C ${1}  rcv
 scp rcv.tar.gz pi@${2}:~
 sudo rm -f rcv.tar.gz
+ssh pi@${2} 'sudo rm /var/log/daemon.log'
 ssh pi@${2} 'sudo systemctl stop rcv.service'
 ssh pi@${2} 'sudo rm -rf rcv'
 ssh pi@${2} 'sudo tar -xzf rcv.tar.gz'
 ssh pi@${2} 'sudo rm -f rcv.tar.gz'
+ssh pi@${2} 'sudo rcv/install.sh'
 ssh pi@${2} 'sudo systemctl daemon-reload'
 ssh pi@${2} 'sudo systemctl start rcv.service'
