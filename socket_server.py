6d918629eb8ef38b5ca8c11116150e0eed56f1c7
Harel <harel.games@gmail.com>
2019-04-02 06:19:44 +0300

    added a simple server to check if raspberry can connect
    Signed-off-by: Harel <harel.games@gmail.com>

---
+++ socket_server.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import socket
+
+HOST = '192.168.2.100'  # Standard loopback interface address (localhost)
+PORT = 5801        # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
